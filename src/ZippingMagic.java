18defc511f534e2e19c3907a18935c3f20e97d62
Pranav Jain <pranav11075@iiitd.ac.in>
2015-03-31 13:40:20 +0530

    Main Class

---
+++ src/ZippingMagic.java
@@ -0,0 +1,24 @@
+import zippingmagic.*;
+import javax.swing.SwingUtilities;
+
+
+/**
+ *
+ * @author Pranav Jain
+ */
+public class ZippingMagic 
+{
+    
+    public static void main(String[] args) 
+    {
+        SwingUtilities.invokeLater(new Runnable() {
+            
+            @Override
+            public void run() 
+            {
+                GUI ex = new GUI();
+		            ex.setVisible(true);
+            }
+        });
+    }
+}
